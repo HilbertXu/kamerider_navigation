0427a9c3ae501c84f18a02f26a22c54d4cb58515
HilbertXu <hilbertxu@outlook.com>
2018-12-16 13:20:20 +0800

    add turn robot action

--- src/turn_robot_server.cpp
+++ src/turn_robot_server.cpp
@@ -2,7 +2,7 @@
 #include <string.h>
 #include <cmath>
 #include <actionlib/server/simple_action_server.h>
-#include <std_msgs/string.h>
+#include <std_msgs/String.h>
 #include <geometry_msgs/Twist.h>
 #include <navigation/turn_robotAction.h>
 
@@ -27,14 +27,14 @@ protected:
     ros::Publisher twist_pub_ = nh_.advertise<geometry_msgs::Twist>("cmd_vel", 1);
 
     //声明用来发布 feedback/result 的消息
-    navigation::turn_robotActionFeedback feedback_;
-    navigation::turn_robotActionResult result_;
+    navigation::turn_robotFeedback feedback_;
+    navigation::turn_robotResult result_;
 
 public:
     
     //构造函数
     turn_robot_action(std::string name) :
-        as_(ng_, name, boost::bind(&turn_robot_action::executeCallback, this, _1), false),
+        as_(nh_, name, boost::bind(&turn_robot_action::executeCallback, this, _1), false),
         action_name_(name)
         {
             as_.start();
@@ -44,9 +44,10 @@ public:
 
     //执行回调函数，在收到来自客户端的请求的时候执行操作
     //将会由客户端传来一个目标转动角度
-    void executeCallback (const navigation::turn_robotActionGoalConstPtr& goal)
+    void executeCallback (const navigation::turn_robotGoalConstPtr& goal)
     {
         ROS_INFO("%s: Executing, setting goal twist angle %i", action_name_.c_str(), goal->goal_angle);
+        bool success = true;
         double rate = 50;
         ros::Rate loopRate (rate);
 
@@ -58,22 +59,99 @@ public:
             float turn_duration = goal->goal_angle / angular_speed;
             int ticks = int (turn_duration * rate);
             
-            if (goal->goal_angle > 0)
+            // check that preempt has not been requested by the client
+            for (int i=0; i<ticks; i++)
             {
-                vel.angular.z = angular_speed;
-                for (int i=0; i<ticks; i++)
+                if (as_.isPreemptRequested() || !ros::ok())
                 {
+                    ROS_INFO ("%s: Preempted", action_name_.c_str());
+
+                    //设置当前action的状态为被抢占
+                    as_.setPreempted();
+                    success = false;
+                    break;
+                }
+
+                if (goal->goal_angle > 0)
+                {
+                    vel.linear.x = 0;
+                    vel.linear.y = 0;
+                    vel.linear.z = 0;
+                    vel.angular.x = 0;
+                    vel.angular.y = 0;
+                    vel.angular.z = angular_speed;
                     twist_pub_.publish (vel);
-                    /*
-                    @TODO
-                    理解什么是抢占注册机制
-                    添加feedback，内容为每次发布twist消息后转动的角度
-                    */
+                    feedback_.current_angle = i*angular_speed;
+                    as_.publishFeedback (feedback_);
+                    loopRate.sleep();
+                }
+
+                if (goal->goal_angle < 0)
+                {
+                    vel.linear.x = 0;
+                    vel.linear.y = 0;
+                    vel.linear.z = 0;
+                    vel.angular.x = 0;
+                    vel.angular.y = 0;
+                    vel.angular.z = -angular_speed;
+                    twist_pub_.publish (vel);
+                    feedback_.current_angle = i*(-angular_speed);
+                    as_.publishFeedback (feedback_);
+                    loopRate.sleep();
                 }
             }
         }
 
+        if (fabs(goal->goal_angle > PI/6))
+        {
+            float angular_speed = 0.5;
+            float turn_duration = goal->goal_angle / angular_speed;
+            int ticks = int (turn_duration * rate);
+            
+            // check that preempt has not been requested by the client
+            for (int i=0; i<ticks; i++)
+            {
+                if (as_.isPreemptRequested() || !ros::ok())
+                {
+                    ROS_INFO ("%s: Preempted", action_name_.c_str());
+
+                    //设置当前action的状态为被抢占
+                    as_.setPreempted();
+                    success = false;
+                    break;
+                }
+
+                vel.linear.x = 0;
+                vel.linear.y = 0;
+                vel.linear.z = 0;
+                vel.angular.x = 0;
+                vel.angular.y = 0;
+                vel.angular.z = 0;
+
+                twist_pub_.publish (vel);
+                feedback_.current_angle = i*angular_speed;
+                as_.publishFeedback (feedback_);
+                loopRate.sleep();
+            }
+        }
+
+        if (success)
+        {
+            result_.final_angle = feedback_.current_angle;
+            ROS_INFO ("%s: Succeeded", action_name_.c_str());
+            as_.setSucceeded(result_);
+        }
+
     }
 
+};
+
+int main(int argc, char** argv)
+{
+    ros::init (argc, argv, "turn_robot_server");
+    turn_robot_action turn_robot("turn_robot_server");
+    ros::spin();
+
+    return 0;
 }
 
